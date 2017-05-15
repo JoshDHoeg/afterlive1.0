47e53cd2c08f93cc5069dec79762cfb3f13f47a1
Josh Hoeg <jdhoeg97@gmail.com>
2017-05-14 20:23:24 -0700

    I added some style changes and updated the links throughout the website Almost ready to update the real website

---
+++ virtualenvs/FPScript.sql
@@ -0,0 +1,1 @@
+INSERT INTO `mydb`.`Festival_has_Photographer` (`Festival_Festival_ID`, `Photographer_Photographer_ID`, `Photographer_Festival_ID`) VALUES ('2', '9', '1');
---
+++ virtualenvs/FestivalScript.sql
@@ -0,0 +1,6 @@
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('1', 'XGames', '2017-01-27', '2017-01-29', '238', '1');
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('2', 'Mysteryland USA', '2016-06-10', '2016-06-13', '239', '2');
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('3', 'GemAndJam', '2017-02-02', '2017-02-05', '240', '3');
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('4', 'Euphoria', '2017-04-07', '2017-04-10', '241', '4');
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('5', 'Decadence', '2016-12-31', '2017-01-01', '242', '5');
+INSERT INTO `mydb`.`Festival` (`Festival_ID`, `Name`, `Start_Date`, `End_Date`, `Festival_Account_id`, `Festival_Venue_id`) VALUES ('6', 'Middlelands', '2017-5-5', '2017-7-7', '243', '6');
